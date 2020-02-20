c766e737460e72d2cd52a459eb3597da8da6749f
James Downer <downer.james.256k@gmail.com>
2020-02-20 15:05:45 -0700

    Create barebones V8 runtime

--- app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
+++ app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
@@ -1,14 +1,27 @@
 package io.github.ascenderx.mobilescript
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.eclipsesource.v8.JavaVoidCallback
+import com.eclipsesource.v8.V8
+import com.eclipsesource.v8.V8Array
+import com.eclipsesource.v8.V8Object
+import io.github.ascenderx.mobilescript.ui.home.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HomeFragment.Evaluator {
+    private lateinit var v8: V8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +38,18 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // init v8
+        v8 = V8.createV8Runtime()
+    }
+
+    override fun evaluate(text: String) {
+        v8.executeScript(text)
+    }
+
+    override fun getRuntime() : V8 {
+        return v8
     }
 }
+
+
--- app/src/main/java/io/github/ascenderx/mobilescript/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/ascenderx/mobilescript/ui/home/HomeFragment.kt
@@ -1,18 +1,38 @@
 package io.github.ascenderx.mobilescript.ui.home
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.eclipsesource.v8.JavaVoidCallback
+import com.eclipsesource.v8.V8
+import com.eclipsesource.v8.V8Array
+import com.eclipsesource.v8.V8Object
 import io.github.ascenderx.mobilescript.R
+import java.lang.Exception
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var textView: TextView
+    private lateinit var v8: V8
+    private lateinit var evaluator: Evaluator
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is Evaluator) {
+            evaluator = context
+            v8 = evaluator.getRuntime()
+            v8.registerJavaMethod(PrintLineCallback(this), "println")
+            Log.d("MS.Home.attach", context.toString())
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +42,37 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        textView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(this, Observer {
+//            textView.text = it
+//        })
+        val editText: TextView = root.findViewById(R.id.editText)
+        val btRun: Button = root.findViewById(R.id.bt_run)
+
+        btRun.setOnClickListener { view ->
+            try {
+                this.evaluator.evaluate(editText.text.toString())
+            } catch (ex: Exception) {
+                write(ex.message.toString())
+            }
+        }
         return root
+    }
+
+    fun write(text: String) {
+        textView.text = text
+    }
+
+    interface Evaluator {
+        fun getRuntime(): V8
+        fun evaluate(text: String)
+    }
+}
+
+class PrintLineCallback(private val fragment: HomeFragment) : JavaVoidCallback {
+
+    override fun invoke(receiver: V8Object, parameters: V8Array) {
+        val argument: String = parameters[0] as String
+        fragment.write(argument)
     }
 }
