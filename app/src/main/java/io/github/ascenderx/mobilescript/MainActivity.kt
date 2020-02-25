28060c418e7d5ea1835cce807a0bce0ea841cca4
James Downer <downer.james.256k@gmail.com>
2020-02-25 16:11:43 -0700

    Hide menu kebab button

--- app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
+++ app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
@@ -27,15 +27,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_console), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+        // menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
