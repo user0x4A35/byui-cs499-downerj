eaba584fce9a7b1546413612465ec696902469dd
James Downer <downer.james.256k@gmail.com>
2020-02-25 16:08:07 -0700

    Add custom appbar (provided)

--- app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
+++ app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val toolbar: Toolbar = findViewById(R.id.toolbar)
-//        setSupportActionBar(toolbar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
