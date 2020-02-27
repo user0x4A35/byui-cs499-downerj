e37d42d38e72b0ded353e51ab94b7628385c8f72
James Downer <downer.james.256k@gmail.com>
2020-02-27 02:47:09 -0700

    Add (non-func) reset + file open

--- app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
+++ app/src/main/java/io/github/ascenderx/mobilescript/MainActivity.kt
@@ -1,6 +1,8 @@
 package io.github.ascenderx.mobilescript
 
-import android.content.res.AssetManager
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,15 +10,16 @@ import android.os.Message
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import com.google.android.material.navigation.NavigationView
 import io.github.ascenderx.mobilescript.models.scripting.ScriptEngine
 import io.github.ascenderx.mobilescript.models.scripting.ScriptEventEmitter
 import io.github.ascenderx.mobilescript.models.scripting.ScriptEventListener
@@ -27,7 +30,12 @@ import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity(),
     ScriptEventEmitter {
+    companion object {
+        val REQUEST_GET_CONTENT = 1
+    }
+
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navView: NavigationView
     override lateinit var engine: ScriptEngine
     private val listeners: MutableList<ScriptEventListener> = mutableListOf()
     private val handler: Handler = object : Handler(Looper.getMainLooper()) {
@@ -45,13 +53,21 @@ class MainActivity : AppCompatActivity(),
         setSupportActionBar(toolbar)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_console), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.nav_open -> showScriptOpenDialog()
+                }
+                return true
+            }
+        })
 
         // Start up the scripting engine.
         initScriptEngine()
@@ -70,6 +86,11 @@ class MainActivity : AppCompatActivity(),
                 handler.sendMessage(message)
                 true
             }
+            R.id.action_reset -> {
+                val message: Message = handler.obtainMessage(ScriptMessageStatus.RESTART.value)
+                handler.sendMessage(message)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -77,6 +98,34 @@ class MainActivity : AppCompatActivity(),
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_GET_CONTENT && resultCode == Activity.RESULT_OK) {
+            val fileUri: Uri = data?.data ?: return
+            try {
+                val source: String = loadScriptFromContentUri(fileUri)
+                // Tell the UI to update for restart.
+                val message: Message = handler.obtainMessage(
+                    ScriptMessageStatus.RUN_SCRIPT.value,
+                    source
+                )
+                handler.sendMessage(message)
+            } catch (ex: IOException) {
+                Log.e("MS.main.openScript", ex.message as String)
+            }
+        }
+    }
+
+    private fun showScriptOpenDialog() {
+        val intent = Intent()
+        intent.type = "*/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(
+            Intent.createChooser(intent, "Select a file"),
+            REQUEST_GET_CONTENT
+        )
     }
 
     private fun initScriptEngine() {
@@ -88,25 +137,42 @@ class MainActivity : AppCompatActivity(),
 
     private fun loadScriptAssets(): List<String> {
         val sources: MutableList<String> = mutableListOf()
-        val paths: Array<String>? = assets.list("sources")
-        if (paths != null) {
-            for (path in paths) {
+        val fileNames: Array<String>? = assets.list("sources")
+        if (fileNames != null) {
+            for (fileName in fileNames) {
                 try {
-                    val stream: InputStream = assets.open("sources/$path")
-                    val reader = InputStreamReader(stream)
-                    val buffer = StringBuffer()
-                    var ch: Int = reader.read()
-                    while (ch >= 0) {
-                        buffer.append(ch.toChar())
-                        ch = reader.read()
-                    }
-                    sources.add(buffer.toString())
+                    sources.add(loadScript("sources/$fileName"))
                 } catch (ex: IOException) {
                     Log.e("MS.main.loadScripts", ex.message as String)
                 }
             }
         }
         return sources
+    }
+
+    private fun loadScriptFromContentUri(uri: Uri): String {
+        val stream: InputStream = contentResolver.openInputStream(uri)
+            ?: throw IOException("Content stream failed to open")
+        val reader = InputStreamReader(stream)
+        val buffer = StringBuffer()
+        var ch: Int = reader.read()
+        while (ch >= 0) {
+            buffer.append(ch.toChar())
+            ch = reader.read()
+        }
+        return buffer.toString()
+    }
+
+    private fun loadScript(path: String): String {
+        val stream: InputStream = assets.open(path)
+        val reader = InputStreamReader(stream)
+        val buffer = StringBuffer()
+        var ch: Int = reader.read()
+        while (ch >= 0) {
+            buffer.append(ch.toChar())
+            ch = reader.read()
+        }
+        return buffer.toString()
     }
 
     override fun attachScriptEventListener(listener: ScriptEventListener) {
